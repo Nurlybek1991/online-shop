6fe21f5a4df02bebd010293ee46ef3c539a76e2e
User1991 <nurlan19911991a@gmail.com>
2023-11-27 20:10:25 +0600

    Realize routing

--- app/public/index.php
+++ app/public/index.php
@@ -1,1 +1,16 @@
 <?php
+
+$reguestUri = $_SERVER['REQUEST_URI'];
+
+
+if($reguestUri === '/registrate') {
+    require_once './handler/registrate.php';
+} elseif ($reguestUri === '/login') {
+    require_once './handler/login.php';
+} elseif ($reguestUri === '/main') {
+    require_once './handler/main.php';
+} elseif ($reguestUri === '/add-product') {
+    require_once './handler/add-product.php';
+} else {
+    require_once './html/404.html';
+}
